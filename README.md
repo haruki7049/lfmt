e0988d85b55a1439bdb7846dbcc9cc234a40ee5b
haruki7049 <tontonkirikiri@gmail.com>
2024-10-22 22:04:14 +0900

    Edit flake.nix to use flake-parts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lisp-fmt-rs
+
 A CLI code-formatter written in Rust-lang which is inspired by [lisp-format](https://github.com/eschulte/lisp-format), for any lisp dialect.
--- flake.nix
+++ flake.nix
@@ -1,76 +1,109 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
     crane.url = "github:ipetkov/crane";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix, rust-overlay, crane }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ (import rust-overlay) ]; };
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
-        src = ./.;
-        cargoArtifacts = craneLib.buildDepsOnly {
-          inherit src;
-        };
-        lfmt = craneLib.buildPackage {
-          inherit src cargoArtifacts;
-          strictDeps = true;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-          doCheck = true;
-        };
-        cargo-clippy = craneLib.cargoClippy {
-          inherit src cargoArtifacts;
-          cargoClippyExtraArgs = "--verbose -- --deny warnings";
-        };
-        cargo-doc = craneLib.cargoDoc {
-          inherit src cargoArtifacts;
-        };
-        llvm-cov-text = craneLib.cargoLlvmCov {
-          inherit cargoArtifacts src;
-          cargoExtraArgs = "--locked";
-          cargoLlvmCovCommand = "test";
-          cargoLlvmCovExtraArgs = "--text --output-dir $out";
-        };
-        llvm-cov = craneLib.cargoLlvmCov {
-          inherit cargoArtifacts src;
-          cargoExtraArgs = "--locked";
-          cargoLlvmCovCommand = "test";
-          cargoLlvmCovExtraArgs = "--html --output-dir $out";
-        };
-      in
-      {
-        formatter = treefmtEval.config.build.wrapper;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-        packages.default = lfmt;
-        packages.doc = cargo-doc;
-        packages.llvm-cov = llvm-cov;
-        packages.llvm-cov-text = llvm-cov-text;
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          overlays = [ inputs.rust-overlay.overlays.default ];
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+          src = lib.cleanSource ./.;
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+          };
+          lfmt = craneLib.buildPackage {
+            inherit src cargoArtifacts;
+            strictDeps = true;
+            doCheck = true;
+          };
+          cargo-clippy = craneLib.cargoClippy {
+            inherit src cargoArtifacts;
+            cargoClippyExtraArgs = "--verbose -- --deny warning";
+          };
+          cargo-doc = craneLib.cargoDoc {
+            inherit src cargoArtifacts;
+          };
+          llvm-cov = craneLib.cargoLlvmCov {
+            inherit cargoArtifacts src;
+            cargoExtraArgs = "--locked";
+            cargoLlvmCovCommand = "test";
+            cargoLlvmCovExtraArgs = "--text --output-dir $out";
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.default;
+          checks = {
+            inherit
+              lfmt
+              cargo-clippy
+              cargo-doc
+              llvm-cov
+              ;
+          };
+
+          packages = {
+            inherit
+              lfmt
+              cargo-clippy
+              cargo-doc
+              llvm-cov
+              ;
+            default = lfmt;
+            doc = cargo-doc;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+            programs.taplo.enable = true;
+            programs.actionlint.enable = true;
+            programs.mdformat.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.nil
+            ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]\$ "
+            '';
+          };
         };
-
-        checks = {
-          inherit lfmt cargo-clippy cargo-doc llvm-cov llvm-cov-text;
-          formatting = treefmtEval.config.build.check self;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            rust
-            pkgs.nil
-          ];
-
-          shellHook = ''
-            export PS1="\n[nix-shell:\w]$ "
-          '';
-        };
-      }
-    );
+    };
 }
