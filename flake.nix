a1da541e7977c9be0f3c0dfc56ce2e7497edc5ba
haruki7049 <tontonkirikiri@gmail.com>
2024-10-22 22:22:51 +0900

    chore(flake-compat): Setup

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     crane.url = "github:ipetkov/crane";
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
